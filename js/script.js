db9b90078a720817becb29c6e6ab9a93d67b0d57
Sazith Shyonton <68244633+sazithshyonton@users.noreply.github.com>
2020-10-23 20:30:10 +0600

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,16 @@
+function textChanger () {
+	document.getElementById('h2').innerHTML = "Hello SSB";
+}
+
+function textChangertwo () {
+	document.getElementById('two').innerHTML = "Hello SSB";
+}
+
+
+function textChangerthree () {
+	document.getElementById('three').innerHTML = "Hello SSB";
+}
+
+function textChangerfour () {
+	document.getElementById('four').innerHTML = "Hello SSB";
+}
